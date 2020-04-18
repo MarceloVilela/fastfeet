da2101675841ddd79a17ca1518f059a88f6cbabd
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2020-04-18 09:31:43 -0300

    backend - seguindo passo a passo do readme

--- backend/migrations/20200312083526-addColumn-street_at-recipients.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.addColumn('recipients', 'street', {
-    type: Sequelize.STRING,
-  }),
-
-  down: (queryInterface) => queryInterface.removeColumn('recipients', 'street'),
-};

fafea7d5bf9a14980afecc80f311db4dc09c45e5
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2020-04-19 12:17:02 -0300

    frontend - console warning

--- frontend/src/pages/SignOut/index.js
+++ frontend/src/pages/SignOut/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { signOut } from '~/store/modules/auth/actions';
 
@@ -8,7 +8,7 @@ export default function SignOut() {
 
   useEffect(() => {
     dispatch(signOut());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>Saindo...</div>
